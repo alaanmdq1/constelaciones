cd501b51b838569aa0f4c49276062ee706f042e3
Alan Nicolas Ramil <ShowTime@MacBook-Pro-de-Alan.local>
2021-01-18 20:36:58 -0300

    Data Base conection

---
+++ database/util/createDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const dotEnv = require('dotenv')
+
+dotEnv.config()
+
+const { MONGO_DB_URL} = process.env
+
+module.exports.connection = async () => {
+    try {
+        mongoose.set('debug', true)
+        await mongoose.connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+        console.log('Database connected Succesfully')
+    } catch (error) {
+        return console.log(error)
+        throw new Error(error)
+    }
+    
+}
--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const {PORT} = process.env
 const app = require('./app/createExpressApp')
 
 const http = require('http')
+const { connection } = require('./database/util/createDB')
+
+//db connect
+
+connection()
 //servidor creado y escuchando
 http.createServer(app).listen(PORT, () => console.log('server ready'))
