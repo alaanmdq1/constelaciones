8a5168887aeb3d7903f5e974edecacf0323c89e0
Alan Nicolas Ramil <ShowTime@MacBook-Pro-de-Alan.local>
2021-02-02 16:33:13 -0300

    socket connected

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const {PORT, MONGO_DB_URL} = process.env
 const http = require('http')
+const socketio = require('socket.io')
 const createExpressApp = require('./app/createExpressApp')
 //db conexion
 require('./database/util/createDB')()
@@ -8,9 +9,23 @@ require('./database/util/createDB')()
         console.log('Database connected Succesfully')
         //express se integra con la data base
         const app = createExpressApp({ db })
-        //const server = http.createServer(app)
+        const server = http.createServer(app)
         //servidor creado y escuchando
-        http.createServer(app).listen(PORT, () => console.log(`server ready on ${PORT}`))
+        server.listen(PORT, () => console.log(`server ready on ${PORT}`))
+        //web sockets recibe el servidor como parametro
+        const io = socketio(server)
+        //socket connection
+        io.on('connection', socket => {
+            console.log('un paciente o el administrador se ha conectado')
+            //socket escucha evento drag & drop
+            socket.on('drag and drop', drag => {
+                io.emit('drag an drop', drag)
+            })
+            //socket escucha evento disconect
+            socket.on('disconect', () => {
+                console.log('el paciente o el administrado se ha desconectado')
+            })
+        })
     })
 
 
