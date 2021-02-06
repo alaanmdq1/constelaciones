b191fe56bca52099b30b6e94228b18610615d8ba
Alan Nicolas Ramil <ShowTime@MacBook-Pro-de-Alan.local>
2021-02-06 19:05:02 -0300

    actualizando endpoint admin

--- app/rest/v1/administrador/postAdmin.js
+++ app/rest/v1/administrador/postAdmin.js
@@ -1,12 +1,25 @@
 const {Router} = require('express')
 const {adminSchema} = require('../../../../database/schemas/Administrador')
+const {check, validationResult} = require('express-validator')
 
-module.exports = Router().post('/rest/v1/:administrador', async (req, res) => {
-    const {administrador} = req.params
+//creando administrador
+module.exports = Router().post('/rest/v1/:administrador',[
+    //chekea validacion
+    check('nombre').isLength({min: 3}),
+    check('apellido').isLength({min: 3}),
+    check('email').isEmail(),
+    check('password').isStrongPassword()
+], 
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.status(422).json({errors: errors.array()})
+        }
+        const {administrador} = req.params
    
-    const {password} = req.body
-    const db = req.db
-    const usuarioAdmin = new adminSchema ({
+        const {password} = req.body
+        const db = req.db
+        const usuarioAdmin = new adminSchema ({
         nombre: nombre,
         apellido: apellido,
         email: email,
