a7e69d1d7344eb9678d6d1ed178e4746f5527a35
Alan Nicolas Ramil <ShowTime@MacBook-Pro-de-Alan.local>
2021-02-15 19:58:07 -0300

    endpoint login actualizado

--- app/rest/v1/login/postLogin.js
+++ app/rest/v1/login/postLogin.js
@@ -1,9 +1,10 @@
 const {Router} = require('express')
-const Paciente = require('../../../../../database/schemas/Paciente')
+const Paciente = require('../../../../database/schemas/Paciente')
+const Administrador = require('../../../../database/schemas/Administrador')
 const {check, validationResult} = require('express-validator')
 const bcrypt = require('bcrypt')
 
-module.exports = Router().post('/rest/v1/paciente/login',[
+module.exports = Router().post('/rest/v1/login',[
     //chekea validacion
     check('email').isEmail(),
     check('password').isStrongPassword()],
@@ -15,10 +16,14 @@ module.exports = Router().post('/rest/v1/paciente/login',[
         //chekea si el email es correcto
         let usuario = await Paciente.findOne({email: req.body.email})
         if(!usuario) return res.status(400).send('E-mail o contraseña invalido')
+        let administrador = await Administrador.findOne({email: req.body.email})
+        if(!administrador) return res.status(400).send('E-mail o contraseña invalido')
 
         //chekea si la contraseña es correcta
         const validPassword = await bcrypt.compare(req.body.password, usuario.password)
         if(!validPassword) return res.status(400).send('E-mail o contraseña invalido')
+        const validPasswordAdmin = await bcrypt.compare(req.body.password, administrador.password)
+        if(!validPasswordAdmin) return res.status(400).send('E-mail o contraseña invalido')
     
         res.send(`El paciente ${usuario.nombre} ha iniciado sesion con exito` )
     })
