6ba559436fa425ac5a22f5a26815a69e12c73ca9
Alan Nicolas Ramil <ShowTime@MacBook-Pro-de-Alan.local>
2021-02-25 17:48:14 -0300

    endpoints finalizados

--- app/rest/v1/paciente/id/referencia/postReferencia.js
+++ app/rest/v1/paciente/id/referencia/postReferencia.js
@@ -1,19 +1,19 @@
 const {Router} = require('express')
 const Referencia = require('../../../../../../database/schemas/Referencia')
-const Paciente = require('../../../../../../database/schemas/Usuario')
+const Usuario = require('../../../../../../database/schemas/Usuario')
 const authPaciente = require('../../../middleware/authorizationPaciente')
 const authorize = require('../../../middleware/role')
 const role = require('../../../../../helpers/role')
 //crear referencia del tablero
 module.exports = Router().post('/rest/v1/paciente/:id/referencia', [authPaciente, authorize([role.User])] , async (req, res) => {
     
-    const paciente = await Paciente.findById(req.params.id)
+    const paciente = await Usuario.findById(req.params.id)
     if (!paciente) return res.status(400).send('No se ha encontrado el paciente con ese ID')
 
     const referenciaPaciente = new Referencia ({
         nombre: req.body.nombre,
         referencia: req.body.referencia,
-        paciente: req.params.id
+        usuario: req.params.id
     })
     try {
         const result = await referenciaPaciente.save()
